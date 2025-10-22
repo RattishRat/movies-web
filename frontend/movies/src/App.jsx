73266276e816ef6b2083db7417086d68dcec2282
23JS1 <23js1@TECHIN.INTERNAL>
2025-10-22 10:25:26 +0300

    fixed nav bar errors

--- frontend/movies/src/App.jsx
+++ frontend/movies/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
